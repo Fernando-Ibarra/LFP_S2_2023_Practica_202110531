bd5c82aa9ee24a5c8ed49cb127a237c2b696c9e4
Fernando-Ibarra <fi94457@gmail.com>
2023-08-22 11:06:28 -0600

    add: manual

--- models/Product.py
+++ models/Product.py
@@ -9,14 +9,3 @@ class Product(object):
         self.amount = amount
         self.price = price
         self.location = location
-class Product(object):
-   name: str
-   amount: int
-   price: float
-   location: str
-   
-   def __init__(self, name, amount, price, location) -> None:
-       self.name = name
-       self.amount = amount
-       self.price = price
-       self.location = location        
